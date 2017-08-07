97895ea31fceda8f0ff964918600273e5f23215e
AleenL <784757555@qq.com>
2017-08-07 18:37:31 +0800

    点击获取城市ID和该城市的天气

--- src/GetTwon.js
+++ src/GetTwon.js
@@ -15,12 +15,64 @@ class GetTwon extends React.Component{
 	}
 
 	getWeather(event){
-		console.log(event.target.innerText)
-		console.log(event.target.getAttribute('class'))
+		let en=event.target.getAttribute('class'),ch=event.target.innerText
+		var that = this
+		this.getJSON('https://weixin.jirengu.com/weather/cityid','get',{location:en},true).then(function(response){
+				console.log(response)
+				that.getCityId(response,ch)
+			},function(error){
+				console.error('失败')
+		})
+	}
+
+
+	getJSON(url,type,param,async,header){
+		return new Promise(function(resolve,reject){
+			let req = new XMLHttpRequest()
+			req.onload = function(){
+				if(req.status === 200 || req.status === 304){
+					resolve(JSON.parse(req.response))
+				}else{
+					reject(Error(req.statusText))
+				}
+			}
+			req.onerror = function(){
+				reject(Error('NetWork Error'))
+			}
+			//确定默认值
+			type === null || type.toUpperCase() === 'GET' ? type='get' : type = 'post'
+			param = formatParams(param)
+			param === null || param === '' ? url : url=url+'?'+param
+			async === null || async === true ? async=true : async=false
+			//设置提交表单内容
+
+			req.open(type,url,async)
+			req.send()
+		});
+		function formatParams(data){
+			var _fpArr = [];
+			for (let _fpName in data) {
+				_fpArr.push(_fpName + '=' + data[_fpName])
+			}
+			return _fpArr.join('&')
+		}
+	}
+
+	getCityId(Json,name){
+		for(let i=0;i<Json.results.length;i++){
+			if(Json.results[i].name === name){
+				this.getJSON('https://weixin.jirengu.com/weather/now','get',{cityid:Json.results[i].id},true).then(
+					function(response){
+						console.log(response)
+					},function(error){
+						console.error('失败')
+					}
+				)
+			}
+		}
 	}
 
 	render(){
-		console.log(this.state);
 		return(
 			<ul>
 				{this.state.getTwon.map(function(value,index){
