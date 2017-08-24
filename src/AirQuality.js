9597833cd4e419f01d9dbd81fb8fdf84fdcc8d04
AleenL <784757555@qq.com>
2017-08-24 20:28:21 +0800

    第二个模块完成

---
+++ src/AirQuality.js
@@ -0,0 +1,77 @@
+import React from 'react'
+
+class AirQuality extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state={
+			air:this.props.air,
+			face:null,
+			text:null,
+		}
+	}
+
+	
+	componentWillUpdate(nextProps) {
+		if(this.props.air !== nextProps.air){
+			this.changeFace(this.props.air.city.aqi)
+		}
+	}
+
+	componentDidMount(){
+		if(this.state.air){
+			this.changeFace(this.props.air.city.aqi)
+		}
+	}
+
+	changeFace(node){
+		if(parseInt(node)<=50){
+			this.setState({
+				face:'good',
+				text:'优',
+				suggestText:'空气清新，适合外出登山踏青，温度过高时请记得擦涂防晒霜，温度过低时请注意添衣保暖。'
+			})
+		}else if(parseInt(node)<=100 && parseInt(node)>50){
+			this.setState({
+				face:'good',
+				text:'良好',
+				suggestText:'空气良好，是个聚会的好时光，和两三朋友小聚一次，小酌一杯清酒，岂不美哉？'
+			})
+		}else if(parseInt(node)<=150 && parseInt(node)>100){
+			this.setState({
+				face:'bad',
+				text:'轻度污染',
+				suggestText:'空气污染加重，好好宅在家里面，在冰箱里拿出一罐啤酒，配上炸鸡，再挑一部好看的电影。'
+			})
+		}else{
+			this.setState({
+				face:'bad',
+				text:'重度污染',
+				suggestText:'严重的空气污染，出行请带好口罩，防止呼吸道感染等疾病，未雨绸缪总是好的！'
+			})
+		}
+	}
+
+	render(){
+		return(
+			<div className='airBox'>
+				<div className='airTitle'>
+					<p><i className={`iconfont ${this.state.face}`}></i></p>
+					<p>空气指数 : {this.state.air.city.aqi} {this.state.text}</p>
+				</div>
+				<div className='airSuggest'>
+					<p style={{lineHeight:'1.5rem'}}>{this.state.suggestText}</p>
+					<div className='airNumber'>
+						<p>PM2.5 <b>{this.state.air.city.pm25}</b></p>
+						<p>PM10 <b>{this.state.air.city.pm10}</b></p>
+						<p>SO<sub>2</sub> <b>{this.state.air.city.so2}</b></p>
+						<p>CO <b>{this.state.air.city.co}</b></p>
+						<p>O<sub>3</sub> <b>{this.state.air.city.o3}</b></p>
+						<p>NO<sub>2</sub> <b>{this.state.air.city.no2}</b></p>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default AirQuality
--- src/Suggestion.js
+++ src/Suggestion.js
@@ -9,6 +9,7 @@ import './Suggestion.css'
 import GetTime from './GetTime'
 import WeatherCN from './weatherCN'
 import WindIcon from './WindIcon'
+import AirQuality from './AirQuality'
 
 
 
@@ -23,18 +24,21 @@ const hello = ({location}) =>(
 				<GetTime/>
 				<div className='weatherList'>
 				{console.log(location.state)}
-					<div className='weatherListTitle'>
-						<WeatherCN weather={location.state.today} getIcon='Icon' />
-						<p>{location.state.today.text}</p>
-						<p>{location.state.today.temperature}°C</p>
-						<p>{location.state.suggest.suggestion.sport.brief}运动</p>
+					<div>
+						<div className='weatherListTitle'>
+							<WeatherCN weather={location.state.today} getIcon='Icon' />
+							<p>{location.state.today.text}</p>
+							<p>{location.state.today.temperature}°C</p>
+							<p>{location.state.suggest.suggestion.sport.brief}运动</p>
+						</div>
+						<div className='weatherItems'>
+							<p><i className='iconfont'>&#xe71a;</i>湿度 {location.state.today.humidity}%</p>
+							<p><i className='iconfont'>&#xe62d;</i>体感温度 {location.state.today.feels_like}°</p>
+							<WindIcon wind={location.state.today} />
+							<p><i className='iconfont'>&#xe654;</i>紫外线 {location.state.suggest.suggestion.uv.brief}</p> 
+						</div>
 					</div>
-					<div className='weatherItems'>
-						<p><i className='iconfont'>&#xe71a;</i>湿度 {location.state.today.humidity}%</p>
-						<p><i className='iconfont'>&#xe62d;</i>体感温度 {location.state.today.feels_like}°</p>
-						<WindIcon wind={location.state.today} />
-						<p><i className='iconfont'>&#xe654;</i>紫外线 {location.state.suggest.suggestion.uv.brief}</p> 
-					</div>
+					<AirQuality air={location.state.today.air_quality} />
 				</div>
 			</div>
 		</div>
