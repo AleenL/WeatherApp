73596f90583c2c52d6856c2fc4a929e300105c7b
AleenL <784757555@qq.com>
2017-08-24 10:53:40 +0800

    更新建议背景图片

--- src/Suggestion.js
+++ src/Suggestion.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Link,
+  Switch
 } from 'react-router-dom'
+import './Suggestion.css'
 
 
 
 const hello = ({location}) =>(
 	<div>
 		<div className='userAction'>
-		{console.log(location.state)}
-		<p>{location.state.dressing.details}</p>
-		<p>{location.state.flu.details}</p>
-		<p>{location.state.car_washing.details}</p>
-		<p>{location.state.sport.details}</p>
-		<p>{location.state.travel.details}</p>
+			<div className='ReturnBack'>
+				<p><Link to='/'><i className='iconfont'>&#xe603;</i></Link></p>
+				<p>建议</p>
+			</div>
+			{console.log(location.state)}
+			<p>{location.state.dressing.details}</p>
+			<p>{location.state.flu.details}</p>
+			<p>{location.state.car_washing.details}</p>
+			<p>{location.state.sport.details}</p>
+			<p>{location.state.travel.details}</p>
 		</div>
 	</div>
 )
